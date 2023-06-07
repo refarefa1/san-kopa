3b8ed6565ef00e6e61f3e8b81f93a3f24e9c8da9
Raz Israeli <razisraeli1@gmail.com>
2023-06-07 21:52:20 +0300

    Files structure changes

--- src/views/common/auth/SignupPage.jsx
+++ src/views/common/auth/SignupPage.jsx
@@ -1,7 +1,45 @@
+import { InstructorAdd } from '../../../components/instructor/InstructorAdd'
+import { OrganizationAdd } from '../../../components/organization/OrganizationAdd'
+import { Terms } from '../../../components/common/Terms'
+import { Message } from '../../../components/common/Message'
+
+const IdentificationPage = () => {
+    return (
+        <section className="identification-page-container">
+            <h1>identification page</h1>
+        </section>
+    )
+}
+
+const InstructorSignup = () => {
+    return (
+        <section className="InstructorSignup-page-container">
+            <h1>InstructorSignup page</h1>
+            <InstructorAdd />
+            <Terms />
+            <Message />
+        </section>
+    )
+}
+
+const OrganizationSignup = () => {
+    return (
+        <section className="OrganizationSignup-page-container">
+            <h1>OrganizationSignup page</h1>
+            <OrganizationAdd />
+            <Terms />
+            <Message />
+        </section>
+    )
+}
+
 export const SignupPage = () => {
     return (
         <section className="signup-page-container">
             <h1>Signup page</h1>
+            <IdentificationPage />
+            <InstructorSignup />
+            <OrganizationSignup />
         </section>
     )
 }
--- src/views/common/homepage/HomePage.jsx
+++ src/views/common/homepage/HomePage.jsx
@@ -1,11 +1,29 @@
 import { Link } from "react-router-dom"
 
+const LecturePage = () => {
+    return (
+        <section className="lecture-page-container">
+            <h1>Lecture page</h1>
+        </section>
+    )
+}
+
+const InstructorPage = () => {
+    return (
+        <section className="instructor-page-container">
+            <h1>Instructor page</h1>
+        </section>
+    )
+}
+
 export const HomePage = () => {
     return (
         <section className="home-page-container">
             <h1>Home page</h1>
             <Link to="/login">Login</Link>
             <Link to="/signup">Signup</Link>
+            <LecturePage />
+            <InstructorPage />
         </section>
     )
 }
